358f6bc897514384b8368777adbae5fd2f18e772
Tim Rogers <timrogers@purdue.edu>
2020-12-08 16:21:55 -0500

    no tabs

--- code-examples/ocean/ocean.cc
+++ code-examples/ocean/ocean.cc
@@ -5,13 +5,13 @@
 #define H (10+1)
 
 void sequentialOceanIteration(float in[W][H], float out[W][H]) {
-	for (int i=0; i < W; ++i) {
-		for (int j=0; j < H; ++j) {
-			out[i][j] = 0.2*(in[i][j]+in[i-1][j]+
-					in[i][j-1]+in[i+1][j]+
-					in[i][j+1]);
-		}
-	}
+    for (int i=0; i < W; ++i) {
+        for (int j=0; j < H; ++j) {
+            out[i][j] = 0.2*(in[i][j]+in[i-1][j]+
+                in[i][j-1]+in[i+1][j]+
+                in[i][j+1]);
+        }
+    }
 }
 
 
