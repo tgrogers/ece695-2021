536ce1d00a585fe54cad70cb937cce49be0a1030
tgrogers <timrogers@purdue.edu>
2020-12-08 22:08:16 -0500

    first cut at SIMD test example

--- code-examples/simd-example/simdtest.cu
+++ code-examples/simd-example/simdtest.cu
@@ -1,1 +1,28 @@
 
+#include <iostream>
+
+__global__ void simdtest(float* A) {
+  int globalID = blockIdx.x * blockDim.x + threadIdx.x;
+  if ( (threadIdx.x % 32) < 8 ) {
+    A[globalID] = 1;
+  } else {
+    A[globalID] = 0;
+  }
+}
+
+int main()
+{
+  int THREADS = 256;
+  float* A;
+
+  cudaMallocManaged(&A, THREADS*sizeof(float));
+  
+  std::cout << "launch" << std::endl;
+  simdtest<<< THREADS/256, 256 >>>(A);
+  cudaDeviceSynchronize();
+  std::cout << "A: ";
+  for ( int i = 0; i < THREADS; ++i) {
+    std::cout << A[i] << ", ";
+  }
+  std::cout << std::endl;
+}
