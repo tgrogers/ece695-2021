55ef4e5c515692564b41ef7b8009707c8a23e839
tgrogers <timrogers@purdue.edu>
2020-12-09 22:29:58 -0500

    update to compile

--- code-examples/shmem-gemm/shmem-gemm.cu
+++ code-examples/shmem-gemm/shmem-gemm.cu
@@ -1,4 +1,6 @@
-__global__ void MatrixMulKernel(float* M, float* N, float* P, Int Width)
+#define TILE_WIDTH 32
+
+__global__ void MatrixMulKernel(float* M, float* N, float* P, int Width)
 {
   __shared__ float ds_M[TILE_WIDTH][TILE_WIDTH];
   __shared__ float ds_N[TILE_WIDTH][TILE_WIDTH];
@@ -10,17 +12,36 @@ __global__ void MatrixMulKernel(float* M, float* N, float* P, Int Width)
   int Col = bx * blockDim.x + tx;
   float Pvalue = 0;
 
- // Loop over the M and N tiles required to compute the P element
- for (int p = 0; p < n/TILE_WIDTH; ++p) {
+  // Loop over the M and N tiles required to compute the P element
+  for (int p = 0; p < Width/TILE_WIDTH; ++p) {
     // Collaborative loading of M and N tiles into shared memory
     ds_M[ty][tx] = M[Row*Width + p*TILE_WIDTH+tx];
     ds_N[ty][tx] = N[(p*TILE_WIDTH+ty)*Width + Col];
     __syncthreads();
 
     for (int i = 0; i < TILE_WIDTH; ++i)
-	Pvalue += ds_M[ty][i] * ds_N[i][tx];
+      Pvalue += ds_M[ty][i] * ds_N[i][tx];
     __syncthreads();
-  }	
+  }
   P[Row*Width+Col] = Pvalue;
+}
+
+#include <iostream>
+
+int main()
+{
+  const int size = 1024;
+  float* M;
+  float* N;
+  float* P;
+  cudaMallocManaged(&M, size*size*sizeof(float));
+  cudaMallocManaged(&N, size*size*sizeof(float));
+  cudaMallocManaged(&P, size*size*sizeof(float));
+
+  MatrixMulKernel<<<(size/TILE_WIDTH, size/TILE_WIDTH),
+                    (TILE_WIDTH, TILE_WIDTH)>>>(M,N,P,size);
+  cudaDeviceSynchronize();
+  std::cout << P[0];
+
 }
 
